d2a3864a34b57d984982a3faa4bdf4923fece873
Tarmo Roopärg <50445828+Cukiini@users.noreply.github.com>
2019-09-05 09:55:34 +0300

    Second

---
+++ HelloWorld/HelloWorld/SecondActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloWorld
+{
+    public class SecondActivity : Activity
+    {
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+        }
+    }
+}
